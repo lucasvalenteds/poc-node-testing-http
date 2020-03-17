37c76d175f5df11c4f6c50ad6ea99f520ae9d71d
Lucas <lucasvalenteds@gmail.com>
2020-03-16 21:55:11 -0300

    Add source code

---
+++ index.spec.ts
@@ -0,0 +1,55 @@
+import Axios, { AxiosInstance } from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+import { CustomerAPI, CustomerAPIDefault, Customer, Failure } from ".";
+
+const customer: Customer = {
+  id: 1,
+  name: "John Smith",
+};
+
+const failure: Failure = {
+  message: "Could not find customer with ID " + customer.id,
+};
+
+describe("Using customer instance", () => {
+  const httpClient: AxiosInstance = Axios.create();
+  const mock: AxiosMockAdapter = new AxiosMockAdapter(httpClient);
+  const api: CustomerAPI = new CustomerAPIDefault(httpClient);
+
+  test("It returns customer when ID exists", async () => {
+    mock.onGet("/customer/" + customer.id).reply(200, customer);
+
+    const result = await api.findCustomerById(customer.id);
+
+    expect(result).toStrictEqual(customer);
+  });
+
+  test("It returns failure when ID does not exists", async () => {
+    mock.onGet("/customer/" + customer.id).reply(404);
+
+    const result = await api.findCustomerById(customer.id);
+
+    expect(result).toStrictEqual(failure);
+  });
+});
+
+describe("Using static instance", () => {
+  const mock: AxiosMockAdapter = new AxiosMockAdapter(Axios);
+  const api: CustomerAPI = new CustomerAPIDefault();
+
+  test("It returns customer when ID exists", async () => {
+    mock.onGet("/customer/" + customer.id).reply(200, customer);
+
+    const result = await api.findCustomerById(customer.id);
+
+    expect(result).toStrictEqual(customer);
+  });
+
+  test("It returns failure when ID does not exists", async () => {
+    mock.onGet("/customer/" + customer.id).reply(404);
+
+    const result = await api.findCustomerById(customer.id);
+
+    expect(result).toStrictEqual(failure);
+  });
+});
---
+++ index.ts
@@ -0,0 +1,30 @@
+import Axios, { AxiosInstance, AxiosResponse } from "axios";
+
+export interface Customer {
+  id: number;
+  name: string;
+}
+
+export interface Failure {
+  message: string;
+}
+
+export interface CustomerAPI {
+  findCustomerById(id: number): Promise<Customer | Failure>;
+}
+
+export class CustomerAPIDefault implements CustomerAPI {
+  public constructor(private httpClient: AxiosInstance = Axios.create()) {}
+
+  async findCustomerById(id: number): Promise<Customer | Failure> {
+    try {
+      const response: AxiosResponse<Customer> = await this.httpClient.get(
+        "/customer/" + id
+      );
+
+      return response.data;
+    } catch (error) {
+      return { message: "Could not find customer with ID " + id };
+    }
+  }
+}
